b82c4d764430afe945a1949304222d2425cf8988
a0re <102201308+a0re@users.noreply.github.com>
2024-05-23 22:28:12 +1000

    Started Manage.php Page

--- site/manage.php
+++ site/manage.php
@@ -1,5 +1,34 @@
 <?php include 'header.inc'; ?>
+<?php include 'settings.php'; ?>
 
+    <main>
+        <section class="container">
+            <h1> Manage EOI </h1>
+            <?php
+            echo "<select name='Field' id='cars'>
+                <option value='Reference Number'> Reference Num </option>
+                <option value='First Name'> First Name</option>
+                <option value='Last Name'> Last Name</option>
+                <option value='Full Name'> Full Name</option>
+                <option value='Status'> Status </option>
+                <option value='Status'> Skills </option>
+                </select>
+                "
+            ?>
+            <button> Delete All EOI's</button>
+            <table>
+                <thead>
+                <tr>
+                    <th> #</th>
+                    <th> Name</th>
+                    <th> DOB</th>
+                    <th> Status</th>
+                    <th> Edit</th>
+                </tr>
+                </thead>
+            </table>
+        </section>
+    </main>
 
 
 <?php include 'footer.inc'; ?>
